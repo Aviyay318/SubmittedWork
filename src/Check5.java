b59110ba6a631438cddc40c16e08ff3de5dd5833
Aviya Yechezkel <aviyay@edu.acc.ac.il>
2022-12-11 14:46:07 +0200

    checking

--- src/Check5.java
+++ src/Check5.java
@@ -2,6 +2,7 @@ public class Check5 {
     public static void main(String[] args) {
         System.out.println("omg it's work");
         System.out.println("yosef :)");
+        System.out.println("cmd");
         System.out.println("aviya");
     }
 }
